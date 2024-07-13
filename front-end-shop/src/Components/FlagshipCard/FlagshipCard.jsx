34194a20fa72c374338b6ddcd595aef442dc5a05
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-07-13 12:38:20 +0600

    brand json file image link updated

--- front-end-shop/src/Components/FlagshipCard/FlagshipCard.jsx
+++ front-end-shop/src/Components/FlagshipCard/FlagshipCard.jsx
@@ -11,7 +11,7 @@ const FlagshipCard = () => {
     return (
         <div onClick={handleBrandClick} className='max-w-7xl mx-auto px-1 md:px-0 overflow-hidden hover:cursor-pointer'>
             <div
-                className="max-w-7xl mx-auto h-96 bg-[url('https://i.ibb.co/7WdhrZr/google-pixel-6-6-pro-collage-official-press-image.jpg')] bg-cover hover:scale-105 duration-700"
+                className="max-w-7xl mx-auto h-96 bg-[url('https://i.ibb.co/T2Z0z9W/google-pixel-6-portfolio-shot.jpg')] bg-cover hover:scale-105 duration-700"
             >
                 <div className="p-8 md:p-20 bg-black/50 h-full w-full text-gray-200">
                     <p className="text-4xl first-letter:text-[#0bb7e6]">Google AI in your hand.</p>
